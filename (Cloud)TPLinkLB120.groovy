a82b5a5fd0c519d71984e93d3c0dc0f3424c639d
DaveGut <djgutheinz@outlook.com>
2017-09-07 10:19:55 -0500

    Add files via upload

---
+++ (Cloud)TPLinkLB120.groovy
@@ -0,0 +1,201 @@
+/*
+(BETA) TP-Link LB120 Cloud-connect Device Handler
+
+Copyright 2017 Dave Gutheinz
+
+Licensed under the Apache License, Version 2.0 (the "License"); you 
+may not use this  file except in compliance with the License. You may 
+obtain a copy of the License at:
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+implied. See the License for the specific language governing 
+permissions and limitations under the License.
+
+##### Discalimer:  This Service Manager and the associated Device 
+Handlers are in no way sanctioned or supported by TP-Link.  All  
+development is based upon open-source data on the TP-Link devices; 
+primarily various users on GitHub.com.
+
+##### Notes #####
+1.	This DH is a child device to 'beta' 'TP-Link Connect'.
+2.	This device handler supports the TP-Link LB120 functions.
+3.	Please direct comments to the SmartThings community thread 
+	'Cloud TP-Link Device SmartThings Integration'.
+
+##### History #####
+07-26-2017	-	Initial Prototype Release
+07-28-2017	-	Added uninstalled() to Service Manager to delete 
+			device
+07-28-2017	-	Beta Release
+08-01-2017	-	Updated mode tile to always display circadian, turn 
+			color when circadian.
+08-06-2017	-	Editorial changes.  Added annotations for device 
+			applicability to LB130 EM - the master for other LB 
+			device handlers.
+*/
+
+metadata {
+	definition (name: "TP-LinkLB120", namespace: "beta", author: "Dave Gutheinz") {
+		capability "Switch"
+		capability "Switch Level"
+		capability "Sensor"
+		capability "Actuator"
+		capability "refresh"
+		capability "Color Temperature"
+		attribute "bulbMode", "string"
+		command "setModeNormal"
+		command "setModeCircadian"
+	}
+	tiles(scale:2) {
+		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
+			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+				attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc",
+				nextState:"waiting"
+				attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff",
+				nextState:"waiting"
+				attributeState "waiting", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#15EE10",
+				nextState:"waiting"
+				attributeState "commsError", label: 'Comms Error', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#e86d13",
+				nextState:"waiting"
+			}
+			tileAttribute ("deviceError", key: "SECONDARY_CONTROL") {
+				attributeState "deviceError", label: '${currentValue}'
+			}
+			tileAttribute ("device.level", key: "SLIDER_CONTROL") {
+				attributeState "level", label: "Brightness: ${currentValue}", action:"switch level.setLevel"
+			}
+		}
+		standardTile("refresh", "capability.refresh", width: 2, height: 2,  decoration: "flat") {
+			state ("default", label:"Refresh", action:"refresh.refresh", icon:"st.secondary.refresh")
+		}		 
+		controlTile("colorTempSliderControl", "device.colorTemperature", "slider", width: 4, height: 1, inactiveLabel: false,
+		range:"(2700..6500)") {
+			state "colorTemperature", action:"color temperature.setColorTemperature"
+		}
+		valueTile("colorTemp", "device.colorTemperature", inactiveLabel: false, decoration: "flat", height: 1, width: 2) {
+			state "colorTemp", label: '${currentValue}K'
+		}
+		standardTile("bulbMode", "bulbMode", width: 2, height: 2, decoration: "flat") {
+			state "normal", label:'Circadian', action:"setModeCircadian", backgroundColor:"#ffffff", nextState: "circadian"
+			state "circadian", label:'Circadian', action:"setModeNormal", backgroundColor:"#00a0dc", nextState: "normal"
+		}
+		main("switch")
+		details("switch", "colorTempSliderControl", "colorTemp", "bulbMode", "refresh")
+	}
+}
+
+def installed() {
+	updated()
+}
+
+def updated() {
+	unschedule()
+	runEvery15Minutes(refresh)
+	runIn(2, refresh)
+}
+
+void uninstalled() {
+	def alias = device.label
+	log.debug "Removing device ${alias} with DNI = ${device.deviceNetworkId}"
+	parent.removeChildDevice(alias, device.deviceNetworkId)
+}
+
+//	----- BASIC BULB COMMANDS ------------------------------------
+def on() {
+	sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":1}}}', "commandResponse")
+}
+
+def off() {
+	sendCmdtoServer('{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":0}}}', "commandResponse")
+}
+
+def setLevel(percentage) {
+	percentage = percentage as int
+	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"on_off":1,"brightness":${percentage}}}}""", "commandResponse")
+}
+
+def setColorTemperature(kelvin) {
+	kelvin = kelvin as int
+	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"ignore_default":1,"on_off":1,"color_temp": ${kelvin},"hue":0,"saturation":0}}}""", "commandResponse")
+}
+
+def setModeNormal() {
+	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"mode":"normal"}}}""", "commandResponse")
+}
+
+def setModeCircadian() {
+	sendCmdtoServer("""{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"mode":"circadian"}}}""", "commandResponse")
+}
+
+def commandResponse(cmdResponse){
+	def status =  cmdResponse["smartlife.iot.smartbulb.lightingservice"]["transition_light_state"]
+	parseStatus(status)
+}
+
+//	----- REFRESH ------------------------------------------------
+def refresh(){
+	sendCmdtoServer('{"system":{"get_sysinfo":{}}}', "refreshResponse")
+}
+
+def refreshResponse(cmdResponse){
+	def status = cmdResponse.system.get_sysinfo.light_state
+	parseStatus(status)
+}
+
+//	----- Parse State from Bulb Responses ------------------------
+def parseStatus(status){
+	def onOff = status.on_off
+	if (onOff == 1) {
+		onOff = "on"
+	} else {
+		onOff = "off"
+		status = status.dft_on_state
+	}
+	def level = status.brightness
+	def mode = status.mode
+	def color_temp = status.color_temp
+	log.info "$device.name $device.label: Power: ${onOff} / Brightness: ${level}% / Mode: ${mode} / Color Temp: ${color_temp}K"
+	sendEvent(name: "switch", value: onOff)
+	sendEvent(name: "level", value: level)
+	sendEvent(name: "bulbMode", value: mode)
+	sendEvent(name: "colorTemperature", value: color_temp)
+}
+
+//	----- Send the Command to the Bridge -------------------------
+private sendCmdtoServer(command, action){
+	def appServerUrl = getDataValue("appServerUrl")
+	def deviceId = getDataValue("deviceId")
+	def cmdResponse = parent.sendDeviceCmd(appServerUrl, deviceId, command)
+	String cmdResp = cmdResponse.toString()
+	if (cmdResp.substring(0,5) == "ERROR"){
+		def errMsg = cmdResp.substring(7,cmdResp.length())
+		log.error "${device.name} ${device.label}: ${errMsg}"
+		sendEvent(name: "switch", value: "commsError", descriptionText: errMsg)
+		sendEvent(name: "deviceError", value: errMsg)
+		action = ""
+	} else {
+		sendEvent(name: "deviceError", value: "OK")
+	}	
+	switch(action) {
+		case "commandResponse":
+			commandResponse(cmdResponse)
+			break
+
+		case "refreshResponse":
+			refreshResponse(cmdResponse)
+			break
+
+		default:
+			log.info "Interface Error.  See SmartApp and Device error message."
+	}
+}
+
+//	----- CHILD / PARENT INTERCHANGE TASKS -----
+def syncAppServerUrl(newAppServerUrl) {
+	updateDataValue("appServerUrl", newAppServerUrl)
+	    log.info "Updated appServerUrl for ${device.name} ${device.label}"
+}
